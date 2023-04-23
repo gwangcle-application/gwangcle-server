06728581f70459f4f838a858e788005b1cb3532f
JohnPrk <uo3641493@gmail.com>
2023-04-23 20:30:29 +0900

    feat : 테스트 컨테이너 재사용을 위해 컨테이너를 제외하고 데이터 초기화 세팅 및 인수 테스트 환경 세팅

---
+++ src/test/java/com/gwangcle/study/acceptance/AcceptanceSetUp.java
@@ -0,0 +1,27 @@
+package com.gwangcle.study.acceptance;
+
+import com.gwangcle.study.utils.DatabaseCleanUp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@ActiveProfiles("test")
+@TestPropertySource(locations = "/test-application.properties")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AcceptanceSetUp {
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        databaseCleanup.execute();
+    }
+
+    @Test
+    public void init() {
+    }
+}
